794aa92c716e4e508dcd57b832a5301e1e704811
Luciano-Arex <lucianongougni1@gmail.com>
2025-12-21 13:37:21 -0500

    Add files via upload
    Création de la page liste des clients
    Ajout de list_client.php - affichage des clients
    Page de consultation des clients avec chargement dynamique

---
+++ list_client.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src=" http://code.jquery.com/jquery.min.js"></script>
+    <script src="list_client.js"></script>
+    <title>Liste Client</title>
+</head>
+<body>
+    <div id="block_client">
+        <h1>Liste de client</h1>
+        <ul id="list_client"></ul>
+    </div>
+</body>
+</html>
