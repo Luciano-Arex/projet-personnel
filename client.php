d6bb19d3e4d480c01344ee210615d7619ebf5178
Luciano-Arex <lucianongougni1@gmail.com>
2025-12-21 13:21:10 -0500

    Add files via upload
    Ajout de la page client principale avec structure HTML
    Création de la page d'interface client avec sections activités
    Mise en place du template client.php avec structure de base

---
+++ client.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src=" http://code.jquery.com/jquery.min.js"></script>
+    <script src="client_item.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>page client</title>
+</head>
+<body>
+    <div>
+        <h1>Bonjour <span id="mes_information"></span></h1>
+        <div id="activite_client">
+            <h1>Vos activitées</h1>
+            <ul id="activitee"></ul>
+        </div>
+        <div id="block_activity">
+            <h1>Liste d'activité de la boite</h1>
+            <ul id="list_activity"></ul>
+        </div>
+    </div>
+</body>
+</html>
