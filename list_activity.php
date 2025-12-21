64817fadbff34d3187f4581d10ff00750fbbf2ba
Luciano-Arex <lucianongougni1@gmail.com>
2025-12-21 13:33:43 -0500

    Add files via upload
    Création de la page liste d'activités
    Ajout de list_activity.php - affichage des activités
    Page de consultation des activités avec chargement dynamique

---
+++ list_activity.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src=" http://code.jquery.com/jquery.min.js"></script>
+    <script src="list_activity.js"></script>
+    <title>Liste activity</title>
+</head>
+<body>
+    <div id="block_activity">
+        <h1>Liste de activity</h1>
+        <ul id="list_activity"></ul>
+    </div>
+</body>
+</html>
